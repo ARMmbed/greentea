cfc79fdee01c813d84079eb3fcd181fa28c1ebca
Przemek Wirkus <przemyslaw.wirkus@arm.com>
2015-12-14 00:37:42 +0000

    Add hook_test_end and hook_post_test_end implementation
    Add --hooks command line switch

--- test/mbed_gt_test_parallel.py
+++ test/mbed_gt_test_parallel.py
@@ -156,6 +156,7 @@ class GtOptions:
                  report_fails=False,
                  verbose_test_result_only=False,
                  enum_host_tests=None,
+                 hooks_json=None,
                  verbose=True,
                  version=False):
 
@@ -181,6 +182,7 @@ class GtOptions:
         self.report_fails = report_fails
         self.verbose_test_result_only = verbose_test_result_only
         self.enum_host_tests = enum_host_tests
+        self.hooks_json = hooks_json
         self.verbose = verbose
         self.version = version
 
